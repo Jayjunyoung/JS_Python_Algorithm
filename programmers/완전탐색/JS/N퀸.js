bd10b9c0b33208e63812ccd0e1ff55a30e314e94
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-09-23 15:45:29 +0900

    프로그래머스 N퀸 문제 js로 공부, 추가 공부 예정

---
+++ programmers/완전탐색/JS/N퀸.js
@@ -0,0 +1,32 @@
+//백트래킹 문제 -> 자바스크립트 코테 책 몸 풀기 문제(2)
+function search(n, y, width, diagonal1, diagonal2) {
+  let answer = 0;
+
+  //모든 행에 대해서 퀸의 위치가 결정 된 경우
+  if (y === n) {
+    answer += 1;
+  } else {
+    for (let i = 0; i < n; i++) {
+      if (width[i] || diagonal1[i + y] || diagonal2[i - y + n]) {
+        continue;
+      }
+
+      width[i] = diagonal1[i + y] = diagonal2[i - y + n] = true;
+      answer += search(n, y + 1, width, diagonal1, diagonal2);
+      width[i] = diagonal1[i + y] = diagonal2[i - y + n] = false;
+    }
+  }
+
+  return answer;
+}
+
+function solution(n) {
+  const answer = search(
+    n,
+    0,
+    Array(n).fill(false),
+    Array(2 * n).fill(false),
+    Array(2 * n).fill(false)
+  );
+  return answer;
+}
