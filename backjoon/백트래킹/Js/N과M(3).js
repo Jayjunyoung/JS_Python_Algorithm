c563d7013cd4577636d26afbe5a593a2216bfdd4
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-09-26 10:29:26 +0900

    백준 N과M(3) 문제 Js로 해결

---
+++ backjoon/백트래킹/Js/N과M(3).js
@@ -0,0 +1,33 @@
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "./dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+const [n, m] = input.shift().split(" ").map(Number);
+let result = "";
+
+function solution(n, m) {
+  let seq = [...Array(m).fill(0)];
+
+  const dfs = (k) => {
+    if (k === m) {
+      const arr = [];
+      for (let i = 0; i < m; i++) {
+        arr.push(seq[i]);
+      }
+
+      return (result += `${arr.join(" ")}\n`);
+    }
+
+    for (let i = 1; i <= n; i++) {
+      seq[k] = i;
+      dfs(k + 1);
+    }
+  };
+
+  dfs(0);
+  return result;
+}
+
+console.log(solution(n, m));
