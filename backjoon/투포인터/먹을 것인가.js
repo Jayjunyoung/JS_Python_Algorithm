cdc5157e06ee8573f0a33807d3a34c70b9b42a86
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-10-07 10:00:46 +0900

    백준 먹을 것인가 먹힐 것인가 문제 이분탐색 으로 해결

---
+++ backjoon/투포인터/먹을 것인가.js
@@ -0,0 +1,39 @@
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "./dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+const t = +input.shift(); //테스트케이스 갯수
+let answer = []; //여기 2개의 배열을 담아서 길이 출력
+
+for (let i = 0; i < t; i++) {
+  const [n, m] = input.shift().split(" ").map(Number);
+  const a = input.shift().split(" ").map(Number);
+  const b = input
+    .shift()
+    .split(" ")
+    .map(Number)
+    .sort((a, b) => a - b);
+  let cnt = 0;
+
+  a.forEach((v) => {
+    let min = 0;
+    let max = b.length - 1;
+
+    while (min <= max) {
+      let mid = Math.floor((min + max) / 2);
+
+      if (b[mid] < v) {
+        min = mid + 1;
+      } else {
+        max = mid - 1;
+      }
+    }
+
+    cnt += min;
+  });
+  answer.push(cnt);
+}
+
+console.log(answer.join("\n"));
