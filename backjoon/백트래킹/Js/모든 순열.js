930c8b8ba29547cdaab1dca2be6ea9c224c8cae3
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-10-08 13:26:57 +0900

    백준 모든 순열 문제(백트래킹) js로 해결

---
+++ backjoon/백트래킹/Js/모든 순열.js
@@ -0,0 +1,36 @@
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "./dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+const n = +input.shift();
+let visited = Array.from({ length: n }, () => false);
+let selecedNumber = Array.from({ length: n }, () => 0);
+let answer = "";
+
+const dfs = (depth) => {
+  if (depth === n) {
+    let result = [];
+
+    for (let i = 0; i < n; i++) {
+      result.push(selecedNumber[i]);
+    }
+
+    answer += result.join(" ") + "\n";
+    return;
+  }
+
+  for (let i = 0; i < n; i++) {
+    if (visited[i]) continue;
+
+    selecedNumber[depth] = i + 1;
+    visited[i] = true;
+    dfs(depth + 1);
+    visited[i] = false;
+  }
+};
+
+dfs(0);
+
+console.log(answer);
