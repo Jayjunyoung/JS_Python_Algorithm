ed3651081866ad47f18bc1ea5a1dc0f07f4c5dde
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-10-10 20:12:00 +0900

    프로그래머스 N퀸 문제 다른 풀이 공부

---
+++ programmers/백트래킹/N퀸2.js
@@ -0,0 +1,38 @@
+function possibleFun(board, row) {
+  //전부터 같은 라인 혹은 대각선에 있는지 체크
+  for (var i = 1; i < row; i++) {
+    // 같은 라인에 있는지 체크
+    if (board[i] === board[row]) {
+      return false;
+    }
+    // 같은 대각선에 있는지 체크
+    if (Math.abs(board[i] - board[row]) === Math.abs(i - row)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function solution(n) {
+  let answer = 0;
+  const dfs = (board, row) => {
+    if (row === n) {
+      answer++;
+    } else {
+      for (let i = 1; i <= n; i++) {
+        board[row + 1] = i;
+        if (possibleFun(board, row + 1)) {
+          dfs(board, row + 1);
+        }
+      }
+    }
+  };
+
+  for (let i = 1; i <= n; i++) {
+    let board = Array(n + 1).fill(0);
+    board[1] = i;
+    dfs(board, 1);
+  }
+
+  return answer;
+}
