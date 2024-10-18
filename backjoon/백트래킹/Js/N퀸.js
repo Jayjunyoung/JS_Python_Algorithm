8d861ee36c345feeea48aab72ddf9f85bde4d950
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-10-18 15:28:50 +0900

    백준 n queen 문제 복습

--- backjoon/백트래킹/Js/N퀸.js
+++ backjoon/백트래킹/Js/N퀸.js
@@ -9,9 +9,10 @@ const queens = [];
 let answer = 0;
 
 function possible(x, y) {
-  for (const [a, b] of queens) {
-    //같은 행/열에 있으면 안됌
+  for (let [a, b] of queens) {
+    //같은 행 또는 열에 있으면 false
     if (a === x || b === y) return false;
+    //서로 대각선에 있어도 return false
     if (Math.abs(a - x) === Math.abs(b - y)) return false;
   }
   return true;
