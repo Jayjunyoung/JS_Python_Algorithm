61887c133a62ebe489c63d6df3afbc1a3f96b391
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-11-14 15:01:38 +0900

    프로그래머스 동적계획법 정수 삼각형 문제 2가지 버전으로 풀이

---
+++ programmers/DP/정수 삼각형(다른 풀이).js
@@ -0,0 +1,15 @@
+function solution(triangle) {
+  let answer = 0;
+
+  let dp = triangle[triangle.length - 1].slice();
+  //triangle 배열 마지막 요소를 복사하기 위해 slice 사용
+  //bottom - top 방식
+
+  for (let i = triangle.length - 2; i >= 0; i--) {
+    for (let j = 0; j <= i; j++) {
+      dp[j] = Math.max(dp[j], dp[j + 1]) + triangle[i][j];
+    }
+  }
+
+  return dp[0];
+}
---
+++ programmers/DP/정수 삼각형.js
@@ -0,0 +1,24 @@
+function solution(triangle) {
+  let answer = 0;
+
+  let dp = [];
+  //기존에 dp배열을 1차원 기반으로 0으로 채웠는데 이거때매 하나틀림
+  dp[0] = [triangle[0][0]];
+
+  for (let i = 1; i < triangle.length; i++) {
+    dp[i] = [];
+    for (let j = 0; j <= i; j++) {
+      //맨 왼쪽 끝일 경우
+      if (j === 0) {
+        dp[i][j] = dp[i - 1][j] + triangle[i][j];
+      } else if (j === i) {
+        //맨 오른쪽 끝일 경우
+        dp[i][j] = dp[i - 1][j - 1] + triangle[i][j];
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i - 1][j]) + triangle[i][j];
+      }
+    }
+  }
+
+  return Math.max(...dp[triangle.length - 1]);
+}
