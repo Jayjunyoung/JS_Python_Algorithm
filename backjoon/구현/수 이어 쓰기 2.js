ae7d7c9430c3f54eb1b5c7e22a4804f692967cc6
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-11-19 15:35:22 +0900

    백준 수 이어 쓰기2 구현 문제 해결

---
+++ backjoon/구현/수 이어 쓰기 2.js
@@ -0,0 +1,32 @@
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "./dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+const [n, k] = input.shift().split(" ").map(Number);
+
+function solution(n, k) {
+  let digit = 1; //현재 자릿수
+  let count = 9; //현재 자릿수의 포함된 숫자의 갯수
+  let length = 0; //누적된 자릿수 길이
+
+  while (k > length + digit * count) {
+    length += digit * count;
+    digit++; //다음 자릿수로 이동
+    count *= 10;
+  }
+
+  const offset = k - length - 1;
+  const number = Math.floor(offset / digit) + Math.pow(10, digit - 1);
+  const index = offset % digit;
+  //number에서 몇 번째 자릿수 인지를 계산
+
+  if (number > n) {
+    return -1;
+  }
+
+  return String(number)[index];
+}
+
+console.log(solution(n, k));
