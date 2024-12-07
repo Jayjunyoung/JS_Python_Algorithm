bd27783ea5681c8595f58d9292488b4977412717
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-12-07 11:27:35 +0900

    백준 가르침 문제 복습

--- backjoon/백트래킹/Js/가르침.js
+++ backjoon/백트래킹/Js/가르침.js
@@ -1,3 +1,5 @@
+const { promisify } = require("util");
+
 const input = require("fs")
   .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
   .toString()
@@ -35,7 +37,7 @@ let possible = filteredInput.length; // 배울 수 있는 단어의 초기값
 filteredInput.forEach((w) => {
   if (w.length <= leftover) {
     alpha = [...alpha, ...w]; //여기에는 r 두개가 들어가지만 이후 Set을 통해 중복 제거
-    candidate.push(w); //w는 1차원 배열로 구성되어 있을 것
+    candidate.push(w); //w는 1차원 배열로 구성되어 있을 것 [[r], [r]]
   } else {
     possible--;
   }
@@ -54,6 +56,7 @@ if (alpha.length <= leftover) {
 // r 하나이므로 2^1 = 2
 for (let i = 0; i < 1 << alpha.length; i++) {
   if (countBits(i) === leftover) {
+    //index: 알파벳의 위치
     const learnedAlpha = alpha.filter((_, index) => i & (1 << index)); // 현재 배운 알파벳 조합
     let sum = 0;
 
@@ -73,7 +76,7 @@ console.log(answer);
 function countBits(n) {
   let count = 0;
   while (n > 0) {
-    count += n & 1;
+    count += n & 1; //현재 가장 오른쪽의 비트가 1인지 확인 -> 13일 경우 1이 3개가 오른쪽 끝에 갈 것이므로 count는 3
     n >>= 1;
   }
   return count;
