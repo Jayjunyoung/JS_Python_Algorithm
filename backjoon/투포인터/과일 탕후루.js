99c5d7085df521b2f901863a9151997b60e249d5
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-12-10 19:46:34 +0900

    백준 과일 탕후루 문제(투 포인터, 슬라이딩 윈도우) 해결

---
+++ backjoon/투포인터/과일 탕후루.js
@@ -0,0 +1,37 @@
+const { loadEnvFile } = require("process");
+
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "./dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+const n = +input[0];
+const fruits = input[1].split(" ").map(Number);
+
+let left = 0;
+let right = 0;
+let maxLen = 0;
+const fruitCount = new Map();
+
+while (right < n) {
+  //n이 5이므로 right가 5보다 작을 동안
+  const rightFruit = fruits[right];
+
+  fruitCount.set(rightFruit, (fruitCount.get(rightFruit) || 0) + 1);
+
+  while (fruitCount.size > 2) {
+    //과일의 종류가 2개 초과 일때 실행
+    const leftFruit = fruits[left];
+    fruitCount.set(leftFruit, fruitCount.get(leftFruit) - 1);
+    if (fruitCount.get(leftFruit) === 0) {
+      fruitCount.delete(leftFruit);
+    }
+    left++;
+  }
+
+  maxLen = Math.max(maxLen, right - left + 1);
+  right++;
+}
+
+console.log(maxLen);
