af8b17c02c0ba69ba5e72413d9e6b36abbfd7cc0
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-12-11 11:50:31 +0900

    백준 좋다 문제 투 포인터로 해결

---
+++ backjoon/투포인터/좋다.js
@@ -0,0 +1,40 @@
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "./dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+const n = +input.shift();
+const numberArray = input[0].split(" ").map(Number);
+
+numberArray.sort((a, b) => a - b); // 배열 정렬
+let count = 0;
+
+for (let i = 0; i < n; i++) {
+  //numberArray 배열을 체크하기 위한 인덱스
+  let left = 0;
+  let right = n - 1;
+
+  while (left < right) {
+    if (left === i) {
+      left++;
+      continue;
+    } else if (right === i) {
+      right--;
+      continue;
+    }
+
+    const sum = numberArray[left] + numberArray[right];
+
+    if (sum === numberArray[i]) {
+      count++;
+      break; //for 루프의 다음 반복으로 이동, 즉 다음 타겟 숫자로 이동
+    } else if (sum < numberArray[i]) {
+      left++;
+    } else if (sum > numberArray[i]) {
+      right--;
+    }
+  }
+}
+
+console.log(count);
