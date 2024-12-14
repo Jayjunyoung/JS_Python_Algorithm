86948bb68d6588613ccea6176e3e59472ae73eba
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-12-14 20:03:55 +0900

    백준 좋다 문제 20분 제한 두고 재 풀이 및 정답 완료

--- backjoon/투포인터/좋다.js
+++ backjoon/투포인터/좋다.js
@@ -11,16 +11,16 @@ numberArray.sort((a, b) => a - b); // 배열 정렬
 let count = 0;
 
 for (let i = 0; i < n; i++) {
-  //numberArray 배열을 체크하기 위한 인덱스
   let left = 0;
   let right = n - 1;
-  //i가 다음으로 넘어 갈 때 left 0 right는 9 유지
 
   while (left < right) {
     if (left === i) {
       left++;
       continue;
-    } else if (right === i) {
+    }
+
+    if (right === i) {
       right--;
       continue;
     }
@@ -29,10 +29,10 @@ for (let i = 0; i < n; i++) {
 
     if (sum === numberArray[i]) {
       count++;
-      break; //for 루프의 다음 반복으로 이동, 즉 다음 타겟 숫자로 이동
+      break;
     } else if (sum < numberArray[i]) {
       left++;
-    } else if (sum > numberArray[i]) {
+    } else {
       right--;
     }
   }
