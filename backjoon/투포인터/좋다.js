7f3145b73e598deedb6d1deac961101fda1ee4ec
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-12-17 18:06:15 +0900

    백준 좋다 문제 재 풀이

--- backjoon/투포인터/좋다.js
+++ backjoon/투포인터/좋다.js
@@ -16,11 +16,14 @@ for (let i = 0; i < n; i++) {
 
   while (left < right) {
     if (left === i) {
+      //i가 2, 즉 left, right 인덱스에 해당하는 값의 합이 3인지를 검사해야하는데 left가 인덱스 2에 위치하면 안됌
+      //left자체로 3이 되어버러니까!
       left++;
       continue;
     }
 
     if (right === i) {
+      //위에 주석과 마찬가지 의미
       right--;
       continue;
     }
