e9f90365ab42f63d9e048ed19122e8e3822ecf7c
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-12-17 16:16:28 +0900

    프로그래머스 신고결과 받기 카카오 기출문제 공부

---
+++ programmers/해시/신고결과 받기.js
@@ -0,0 +1,38 @@
+function solution(id_list, report, k) {
+  //정지된 id를 담는 배열
+  let answer = Array.from({ length: id_list.length }, () => 0);
+
+  let report_map = new Map();
+  id_list.forEach((id) => {
+    report_map.set(id, []);
+  });
+  //frodo가 junyoung을 4번 신고하면 1번 신고한거로 간주
+  let report_set = new Set(report);
+  let report_count = new Map();
+
+  report_set.forEach((re) => {
+    const [reporter, reported] = re.split(" ");
+
+    report_map.set(reporter, [...report_map.get(reporter), reported]);
+
+    if (report_count.has(reported)) {
+      report_count.set(reported, report_count.get(reported) + 1);
+    } else {
+      report_count.set(reported, 1);
+    }
+  });
+
+  let report_list = Array.from(report_map.values());
+
+  report_count.forEach((value, key) => {
+    if (value >= k) {
+      report_list.forEach((list, index) => {
+        if (list.includes(key)) {
+          answer[index] += 1;
+        }
+      });
+    }
+  });
+
+  return answer;
+}
