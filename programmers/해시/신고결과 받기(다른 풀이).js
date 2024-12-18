652249749f669cdd40e10364f20e4bad7fb6b4e1
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-12-18 11:11:02 +0900

    프로그래머스 신고결과 받기 문제 복습

--- programmers/해시/신고결과 받기(다른 풀이).js
+++ programmers/해시/신고결과 받기(다른 풀이).js
@@ -6,15 +6,18 @@ function solution(id_list, report, k) {
     const [userId, reportedId] = re.split(" ");
 
     if (reportedUser[reportedId] === undefined) {
-      reportedUser[reportedId] = new Set();
+      reportedUser[reportedId] = new Set(); //신고를 한 사람은 중복 안되게
     }
 
     reportedUser[reportedId].add(userId);
   }
 
+  //신고 당한 사람들의 key를 기준으로 for문 돌려
   for (const reportedId of Object.keys(reportedUser)) {
     if (reportedUser[reportedId].size >= k) {
+      //신고 한 사람이 k명 이상이면
       for (let uid of reportedUser[reportedId]) {
+        //신고 한 사람 데이터를 순회 해
         count[uid] = (count[uid] || 0) + 1;
       }
     }
