e94a95d94b819a4455a293d56ac912292ac9f6ef
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-12-19 12:29:24 +0900

    백준 조합 문제 BigInt, 조합 알고리즘을 활용하여 해결 및 학습

---
+++ backjoon/조합/조합.js
@@ -0,0 +1,24 @@
+const { compileFunction } = require("vm");
+
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "./dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+const [n, m] = input.shift().split(" ").map(Number);
+let result = 0;
+
+function combination(n, m) {
+  let numerator = BigInt(1); // 분자
+  let denominator = BigInt(1); // 분모
+
+  for (let i = 0; i < m; i++) {
+    numerator *= BigInt(n - i);
+    denominator *= BigInt(i + 1);
+  }
+
+  return (numerator / denominator).toString();
+}
+
+console.log(combination(n, m));
