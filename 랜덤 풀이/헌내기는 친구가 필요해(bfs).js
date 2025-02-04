b2f97af7beda2e70444026c7e286d4c53161014a
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2025-02-04 15:19:56 +0900

    헌내기는 친구가 필요해 문제 bfs 접근방식으로도 풀이 완료

---
+++ 랜덤 풀이/헌내기는 친구가 필요해(bfs).js
@@ -0,0 +1,59 @@
+const fs = require("fs");
+
+const input = fs
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+//bfs 기반으로 재 풀이
+
+const [n, m] = input.shift().split(" ").map(Number);
+
+let peopleCount = 0;
+
+const dir = [
+  [-1, 0],
+  [1, 0],
+  [0, -1],
+  [0, 1],
+];
+
+let queue = [];
+const map = input.map((v) => v.trim().split(""));
+let visited = Array.from({ length: n }, () => Array(m).fill(false));
+
+for (let i = 0; i < n; i++) {
+  for (let j = 0; j < m; j++) {
+    if (map[i][j] === "I") {
+      queue.push([i, j]);
+      visited[i][j] = true;
+    }
+  }
+}
+
+while (queue.length > 0) {
+  const [cx, cy] = queue.shift();
+
+  for (let [dx, dy] of dir) {
+    const [nx, ny] = [cx + dx, cy + dy];
+
+    if (
+      nx >= 0 &&
+      nx < n &&
+      ny >= 0 &&
+      ny < m &&
+      !visited[nx][ny] &&
+      map[nx][ny] !== "X"
+    ) {
+      visited[nx][ny] = true;
+      queue.push([nx, ny]);
+
+      if (map[nx][ny] === "P") {
+        peopleCount++;
+      }
+    }
+  }
+}
+
+console.log(peopleCount === 0 ? "TT" : peopleCount);
