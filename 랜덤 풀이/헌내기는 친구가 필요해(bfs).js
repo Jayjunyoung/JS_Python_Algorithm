006c44b94d1856094fa2d2fa67d22c492b888775
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2025-02-04 15:27:13 +0900

    bfs라는 함수를 생성하여 푸는 방식으로 재풀이

--- 랜덤 풀이/헌내기는 친구가 필요해(bfs).js
+++ 랜덤 풀이/헌내기는 친구가 필요해(bfs).js
@@ -23,35 +23,39 @@ let queue = [];
 const map = input.map((v) => v.trim().split(""));
 let visited = Array.from({ length: n }, () => Array(m).fill(false));
 
+const bfs = (startX, startY) => {
+  const queue = [[startX, startY]];
+  visited[startX][startY] = true;
+
+  while (queue.length > 0) {
+    const [cx, cy] = queue.shift();
+
+    for (let [dx, dy] of dir) {
+      const [nx, ny] = [cx + dx, cy + dy];
+
+      if (
+        nx >= 0 &&
+        nx < n &&
+        ny >= 0 &&
+        ny < m &&
+        !visited[nx][ny] &&
+        map[nx][ny] !== "X"
+      ) {
+        visited[nx][ny] = true;
+        queue.push([nx, ny]);
+
+        if (map[nx][ny] === "P") {
+          peopleCount++;
+        }
+      }
+    }
+  }
+};
+
 for (let i = 0; i < n; i++) {
   for (let j = 0; j < m; j++) {
     if (map[i][j] === "I") {
-      queue.push([i, j]);
-      visited[i][j] = true;
-    }
-  }
-}
-
-while (queue.length > 0) {
-  const [cx, cy] = queue.shift();
-
-  for (let [dx, dy] of dir) {
-    const [nx, ny] = [cx + dx, cy + dy];
-
-    if (
-      nx >= 0 &&
-      nx < n &&
-      ny >= 0 &&
-      ny < m &&
-      !visited[nx][ny] &&
-      map[nx][ny] !== "X"
-    ) {
-      visited[nx][ny] = true;
-      queue.push([nx, ny]);
-
-      if (map[nx][ny] === "P") {
-        peopleCount++;
-      }
+      bfs(i, j);
     }
   }
 }
