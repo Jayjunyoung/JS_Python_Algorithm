7ab738d5b97edc3f059c0556c248dc48f26d4b82
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2025-02-17 20:22:46 +0900

    변수명 안 헷갈리게 변경

--- 프로그래머스 lv1 정복/달리기 경주.js
+++ 프로그래머스 lv1 정복/달리기 경주.js
@@ -7,14 +7,14 @@ function solution(players, callings) {
 
   for (let i = 0; i < callings.length; i++) {
     const idx = playerMap[callings[i]];
-    const temp = players[idx - 1];
+    const beforePlayer = players[idx - 1];
 
     players[idx - 1] = callings[i];
-    players[idx] = temp;
+    players[idx] = beforePlayer;
 
     //map의 idx도 갱신
     playerMap[callings[i]] = idx - 1;
-    playerMap[temp] = idx;
+    playerMap[beforePlayer] = idx;
   }
 
   return players;
