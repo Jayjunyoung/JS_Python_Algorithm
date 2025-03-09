d1ef537c3098af10888b0bef5f7d66b7d8933e09
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2025-03-09 17:05:35 +0900

    매개변수 네이밍 변경

--- 프로그래머스 lv1 정복/대충 만든 자판.js
+++ 프로그래머스 lv1 정복/대충 만든 자판.js
@@ -1,7 +1,8 @@
 function solution(keymap, targets) {
   const answer = [];
   const map = {};
-  for (const items of keymap) {
+
+  for (let items of keymap) {
     items
       .split("")
       .map(
@@ -9,10 +10,10 @@ function solution(keymap, targets) {
           (map[item] = map[item] < index + 1 ? map[item] : index + 1)
       );
   }
-  for (const items of targets) {
-    answer.push(
-      items.split("").reduce((cur, item) => (cur += map[item]), 0) || -1
-    );
+
+  for (let items of targets) {
+    answer.push(items.split("").reduce((acc, cur) => acc + map[cur], 0) || -1);
   }
+
   return answer;
 }
