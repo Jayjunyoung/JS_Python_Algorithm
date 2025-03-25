438a9f48250352a5fd4cb6fcd1e65d1e03b9a02a
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2025-03-25 10:57:50 +0900

    백준 소수의 연속합 문제 학습

---
+++ backjoon/투포인터/소수의연속합.js
@@ -0,0 +1,41 @@
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "./dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+const n = +input.shift();
+const isPrime = Array(n + 1).fill(true);
+isPrime[0] = isPrime[1] = false;
+
+//에라스토테네스의 체
+for (let i = 2; i <= Math.sqrt(n); i++) {
+  for (let j = i * i; j <= n; j += i) {
+    isPrime[j] = false;
+  }
+}
+
+//값이 true인 것들만 남겨버리기
+const primes = isPrime
+  .map((v, idx) => (v ? idx : -1))
+  .filter((num, idx) => num !== -1);
+let sum = 0;
+let count = 0;
+let i = 0;
+let j = 0;
+
+while (j < primes.length) {
+  sum += primes[j];
+  while (sum > n) {
+    sum -= primes[i];
+    i++;
+  }
+
+  if (sum === n) {
+    count++;
+  }
+
+  j++;
+}
+
+console.log(count);
