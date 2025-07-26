87cca7c6939ce65e3bfa5a229f0407fb4e40decf
no2junyoung.jung <no2junyoung.jung@navercorp.com>
2025-07-26 11:59:53 +0900

    프로그래머스 등굣길 문제 복습 및 재풀이 완료

--- 등굣길.js
+++ 등굣길.js
@@ -1,32 +1,34 @@
 function solution(m, n, puddles) {
-  const MOD = 1000000007;
-  const dp = Array.from({ length: n + 1 }, () => Array(m + 1).fill(1));
-
+  let dp = Array.from({ length : n + 1 }, () => Array(m + 1).fill(1));
+  const mod = 1000000007;
+  
   dp[1][1] = 1;
-
-  for (let row = 1; row <= n; row++) {
-    for (let col = 1; col <= m; col++) {
-      if (row === 1 && col === 1) continue;
-
-      if (isPuddle(col, row, puddles)) {
-        dp[row][col] = 0;
-      } else {
-        const fromTop = row > 1 ? dp[row - 1][col] : 0;
-        const fromLeft = col > 1 ? dp[row][col - 1] : 0;
-        dp[row][col] = (fromTop + fromLeft) % MOD;
+  
+  const isPuddle = (x, y, puddles) => {    
+      
+      for(const puddle of puddles) {
+          if(x === puddle[0] && y === puddle[1]) {
+              return true;
+          }
       }
-    }
+      
+      return false;
   }
-
-  return dp[n][m];
+  
+  for(let row = 1; row <= n; row++) {
+      for(let col = 1; col <= m; col++) {
+          if(row === 1 && col === 1) continue;
+          
+          if(isPuddle(col, row, puddles)) {
+              dp[row][col] = 0;
+          } else {
+              const fromTop = row > 1 ? dp[row-1][col] : 0
+              const fromLeft = col > 1 ? dp[row][col-1] : 0;
+              
+              dp[row][col] = (fromTop + fromLeft) % mod       
+          }
+      }
+  }
+  
+  return dp[n][m]
 }
-
-// 물웅덩이 확인 함수
-const isPuddle = (x, y, puddles) => {
-  for (const puddle of puddles) {
-    if (x === puddle[0] && y === puddle[1]) {
-      return true;
-    }
-  }
-  return false;
-};
