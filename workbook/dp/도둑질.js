aa22bf757a4cec02c817b9822e9d3b791c6b4c4c
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2025-07-27 22:36:34 +0900

    프로그래머스 도둑질 문제를 해결한다.

--- workbook/dp/도둑질.js
+++ workbook/dp/도둑질.js
@@ -4,6 +4,7 @@ function solution(money) {
   let dp2 = Array(n).fill(0);
   //첫번째 집을 도둑질 하는 경우
   dp1[0] = money[0];
+  //첫번째 집의 금액을 유지
   dp1[1] = money[0];
 
   for (let i = 2; i < n - 1; i++) {
@@ -13,7 +14,7 @@ function solution(money) {
   dp2[1] = money[1];
 
   for (let i = 2; i < n; i++) {
-    dp2[i] = Math.max(dp2[i - 2] + money[i], dp2[i - 1]);
+    dp2[i] = Math.max(dp2[i - 1], dp2[i - 2] + money[i]);
   }
 
   const answer = Math.max(dp1[n - 2], dp2[n - 1]);
