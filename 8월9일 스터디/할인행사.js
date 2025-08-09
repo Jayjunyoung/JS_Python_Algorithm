70506f9dbea41487484386bd0dea2fa5f8828721
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2025-08-09 21:17:29 +0900

    프로그래머스 할인행사 문제를 풀이한다.

---
+++ 8월9일 스터디/할인행사.js
@@ -0,0 +1,25 @@
+function solution(want, number, discount) {
+  let cnt = 0;
+  //각 제품마다 할인된 갯수를 세는 배열
+  let cntArray = Array(want.length).fill(0);
+
+  const equals = (a, b) => JSON.stringify(a) === JSON.stringify(b);
+
+  for (let i = 0; i < discount.length; i++) {
+    let item = discount[i];
+
+    if (want.includes(item)) {
+      cntArray[want.indexOf(item)]++;
+    }
+
+    if (i >= 9) {
+      if (want.includes(discount[i - 10])) {
+        cntArray[want.indexOf(discount[i - 10])]--;
+      }
+
+      if (equals(cntArray, number)) cnt++;
+    }
+  }
+
+  return cnt;
+}
