12f6bb3997ba55b75ec3262d2bfc77d067e03f56
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2025-08-24 22:37:33 +0900

    인덱싱 작성 형식을 변경한다.

--- 8월 23일 스터디/게임개발.js
+++ 8월 23일 스터디/게임개발.js
@@ -1,7 +1,7 @@
 //위상정렬 문제인데 이거는 공부를 하자.. 접근이 안된다
 const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
 
-const n = +input.shift();
+const n = +input[0];
 
 // 각 건물의 선행 건물들을 저장
 const graph = Array.from({ length: n + 1 }, () => []);
@@ -12,19 +12,20 @@ const buildTime = Array(n + 1).fill(0);
 // 각 건물의 완성 시간
 const completeTime = Array(n + 1).fill(0);
 
-for(let i = 0; i < n; i++) {
+for(let i = 1; i <= n; i++) {
 
     const [time, ...preBuildings] = input[i].split(" ").map(Number);
 
-    buildTime[i + 1] = time;
+    buildTime[i] = time;
 
     for(let j = 0; j < preBuildings.length; j++) {
         const prev = preBuildings[j];
         if(prev !== -1) {
-            graph[prev].push(i + 1);
+            // i가 1일 때 graph[1].push(2)가 되겠군
+            graph[prev].push(i);
             // 3 : [4]
             // 4번 입장에서는 진입차수가 오른거지
-            inDegree[i + 1]++;
+            inDegree[i]++;
         }
     }
 }
