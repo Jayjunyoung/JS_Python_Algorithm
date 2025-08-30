c390e09b60f777a1c3611f1e149e241adae58418
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2025-08-30 13:04:58 +0900

    주석을 추가한다.

--- 8월 30일 스터디/합승 택시 요금.js
+++ 8월 30일 스터디/합승 택시 요금.js
@@ -14,6 +14,7 @@ function solution(n, s, a, b, fares) {
         board[y][x] = fee;
     })
     
+    // 플로이드 워셜 알고리즘
     for(let k = 1; k <= n; k++) {
         for(let i = 1; i <= n; i++) {
           for(let j = 1; j <= n; j++) {
@@ -23,8 +24,11 @@ function solution(n, s, a, b, fares) {
         }
     }
     
+    // 두 사람이 합승을 안하고 각자 따로 가는 경우 세팅
+    // 이렇게 해놔야 두 사람이 합승을 하는 경우와 비교하여 갱신 가능
     let answer = board[s][a] + board[s][b];
     for(let i = 1; i <= n; i++) {
+		    // 출발점에서 합승 지점 + 마지막 합승 지점에서 a까지 + 마지막 합승 지점에서 b까지
         const shortest = board[s][i] + board[i][a] + board[i][b];
         answer = Math.min(answer, shortest);
     }
