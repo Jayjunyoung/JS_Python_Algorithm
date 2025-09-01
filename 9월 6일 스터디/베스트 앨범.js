de057338b0eed4fd18aef655a2252f7f95d6ca38
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2025-09-01 23:30:36 +0900

    주석을 추가한다.

--- 9월 6일 스터디/베스트 앨범.js
+++ 9월 6일 스터디/베스트 앨범.js
@@ -29,6 +29,7 @@ function solution(genres, plays) {
         }
         
         current.sort((a,b) => b.playCnt - a.playCnt);
+        // 장르 별로 가장 많이 재생 된 2개를 출력해야하므로 if(i < 2) 조건 추가
         current.forEach((a, i) => {
             if(i < 2) {
                 answer.push(a.index)
