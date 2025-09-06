006a866e0c6dda97e0fc7f1f5f7f97e2e646ae12
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2025-09-06 12:44:09 +0900

    토마토 문제를 재풀이 및 queue에 인덱싱으로 접근하여 시간초과를 해결한다.

--- 9월 6일 스터디/토마토.js
+++ 9월 6일 스터디/토마토.js
@@ -1,5 +1,4 @@
-const input = require("fs").readFileSync("./dev/stdin").toString().trim().split("\n");
-
+const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
 
 const [m, n] = input.shift().split(" ").map(Number);
 const map = input.map((v) => v.split(" ").map(Number));
@@ -11,8 +10,8 @@ map.forEach((v, i) => {
         if(tomato === 1) {
             startNode.push([i, j, 0]);
         }
-    })
-})
+    });
+});
 
 // 방향성 배열
 const dir = [
@@ -20,41 +19,37 @@ const dir = [
     [0, 1],
     [1, 0],
     [0, -1]
-]
+];
 
 const Bfs = (startNode) => {
     const queue = [...startNode];
-    const visited = Array.from({length: n}, () => Array(m).fill(false));
-    let maxDay = 0;
+    let minDay = 0;
+    let front = 0;
 
-    startNode.forEach(([cx, cy, _day]) => {
-        visited[cx][cy] = true;
-    })
-
-    while(queue.length > 0) {
-        const [cx, cy, day] = queue.shift();
-        maxDay = Math.max(maxDay, day);
+    while(front < queue.length) {
+        const [cx, cy, day] = queue[front++];
+        minDay = Math.max(minDay, day);
 
         for(let i = 0; i < 4; i++) {
-            const [nx, ny] = [cx + dir[i][0], cy + dir[i][1]];
-
-            if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+            const nx = cx + dir[i][0];
+            const ny = cy + dir[i][1];
             
-            if(!visited[nx][ny] && map[nx][ny] === 0) {
-                visited[nx][ny] = true;
-                map[nx][ny] = 1; // 토마토를 익게 만들기
+            if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+            if(map[nx][ny] === 0) {  // 익지 않은 토마토만
+                map[nx][ny] = 1;     // 토마토를 익게 만들기
                 queue.push([nx, ny, day + 1]);
             }
         }
     }
 
+    // 모든 토마토가 익었는지 확인
     for(let i = 0; i < n; i++) {
         for(let j = 0; j < m; j++) {
             if(map[i][j] === 0) return -1;
         }
     }
 
-    return maxDay;
-}
+    return minDay;
+};
 
 console.log(Bfs(startNode));
