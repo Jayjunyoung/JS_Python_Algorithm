e82b78a72d288526d8490d3b6037a91690154986
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2025-09-09 23:01:58 +0900

    프로그래머스 신규 아이디 추천 문제를 정규식을 활용해 풀이한다.

---
+++ 9월 13일 스터디/신규 아이디 추천.js
@@ -0,0 +1,13 @@
+function solution(new_id) {
+    const answer = new_id
+    .toLowerCase()
+    .replace(/[^\w-_.]/g, "")
+    .replace(/\.+/g, ".")
+    .replace(/^\.|\.$/g, "")
+    .replace(/^$/g, "a")
+    .slice(0, 15).replace(/\.$/g, "")
+    
+    const len = answer.length;
+    
+    return len > 2 ? answer : answer + answer.charAt(len - 1).repeat(3 - len);
+}
