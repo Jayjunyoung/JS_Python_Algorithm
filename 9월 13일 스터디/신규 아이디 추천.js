c1abf97888eab01db53f46c9647832cf2dc9080e
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2025-09-11 22:44:02 +0900

    프로그래머스 신규 아이디 추천 문제를 복습한다.

--- 9월 13일 스터디/신규 아이디 추천.js
+++ 9월 13일 스터디/신규 아이디 추천.js
@@ -6,8 +6,9 @@ function solution(new_id) {
     .replace(/^\.|\.$/g, "")
     .replace(/^$/g, "a")
     .slice(0, 15).replace(/\.$/g, "")
-    
+
     const len = answer.length;
     
     return len > 2 ? answer : answer + answer.charAt(len - 1).repeat(3 - len);
+
 }
