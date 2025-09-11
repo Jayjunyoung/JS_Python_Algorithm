2f856b80992e062ba8b088f262225b2dda7d42d1
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2025-09-11 22:38:57 +0900

    백준 선 긋기 문제를 복습한다.

--- 9월 13일 스터디/선 긋기.js
+++ 9월 13일 스터디/선 긋기.js
@@ -6,17 +6,19 @@ const n = +input.shift();
 const position = input.map((v) => v.split(" ").map(Number));
 position.sort((a,b) => a[0] - b[0])
 
-let ans = 0;
+let ans = 0
 ans += position[0][1] - position[0][0];
-let cur = position[0][1]; // 초기 끝 점 설정
+let cur = position[0][1];
 
 for(let i = 1; i < n; i++) {
     if(position[i][0] <= cur && position[i][1] > cur) {
-        ans += position[i][1] - cur
-        cur = position[i][1]
-    } else if(position[i][0] > cur) {
-        ans += position[i][1] - position[i][0];
-        cur = position[i][1]
+        ans += position[i][1] - cur;
+        cur = position[i][1];
+    }
+    
+    if(position[i][0] > cur) {
+      ans += position[i][1] - position[i][0];
+      cur = position[i][1];
     }
 }
 
