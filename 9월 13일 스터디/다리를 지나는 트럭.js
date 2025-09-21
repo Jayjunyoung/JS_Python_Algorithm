a37964c242c6a4156b4c3af3dd413728b3488c37
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2025-09-21 19:56:06 +0900

    프로그래머스 다리를 지나는 트럭 문제를 복습한다.

--- 9월 13일 스터디/다리를 지나는 트럭.js
+++ 9월 13일 스터디/다리를 지나는 트럭.js
@@ -11,8 +11,10 @@ function solution(bridge_length, weight, truck_weights) {
     while(bridge_sum > 0) {
         answer++;
         
+        // 다리에서 나갈 때 다리 무게에서 감소
         bridge_sum -= bridge.shift();
-        
+         
+        // 다리에 올라올 트럭 갯수 있고 다리 무게 제한을 넘지않는지 판단
         if(truck_weights.length > 0 && bridge_sum + truck_weights[0] <= weight) {
             bridge_sum += truck_weights[0];
             bridge.push(truck_weights.shift())
@@ -21,5 +23,6 @@ function solution(bridge_length, weight, truck_weights) {
         }
         
     }
+    
     return answer;
 }
