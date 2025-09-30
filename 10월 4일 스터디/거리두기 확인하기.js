7ed00e0f48907fcda264664a03f4bd21d4d89631
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2025-09-30 22:54:45 +0900

    들여쓰기를 적용한다.

--- 10월 4일 스터디/거리두기 확인하기.js
+++ 10월 4일 스터디/거리두기 확인하기.js
@@ -1,5 +1,6 @@
 function solution(places) {
     const result = [];
+    
     for (const place of places) {
       let isSafePlace = 1;
   
@@ -27,31 +28,30 @@ function solution(places) {
   }
   
   function isSafe(place, x, y) {
-      
-      const dir = [
-          [-1, 0],
-          [1, 0],
-          [0, 1],
-          [0, -1]
-      ]
-      
-      for(let [dx, dy] of dir) {
-          
-          const nx = dx + x;
-          const ny = dy + y
-          
-           if (nx >= 0 && nx < 5 && ny >= 0 && ny < 5) {
-               // 한칸 이동했는데 응시자가 있다?
-               // 맨해튼 커리 위배(거리가 2이하면 안됌)
-               if (place[nx][ny] === "P") {
-                  return false;
-                  // 빈 자리 기점으로 다시 주변 인접 좌표 탐색
-               } else if (place[nx][ny] === "O") {
-                 for(let [dx, dy] of dir) {
-                     const nx2 = dx + nx;
-                     const ny2 = dy + ny;
-                     
-                     if (nx2 >= 0 && nx2 < 5 && ny2 >= 0 && ny2 < 5) {
+    const dir = [
+      [-1, 0],
+      [1, 0],
+      [0, 1],
+      [0, -1]
+    ];
+  
+    for (let [dx, dy] of dir) {
+      const nx = dx + x;
+      const ny = dy + y;
+  
+      if (nx >= 0 && nx < 5 && ny >= 0 && ny < 5) {
+        // 한칸 이동했는데 응시자가 있다?
+        // 맨해튼 거리 위배(거리가 2이하면 안됨)
+        if (place[nx][ny] === "P") {
+          return false;
+        }
+        // 빈 자리 기점으로 다시 주변 인접 좌표 탐색
+        else if (place[nx][ny] === "O") {
+          for (let [dx2, dy2] of dir) {
+            const nx2 = dx2 + nx;
+            const ny2 = dy2 + ny;
+  
+            if (nx2 >= 0 && nx2 < 5 && ny2 >= 0 && ny2 < 5) {
               // 현재 응시자의 위치가 아닌 경우
               if (nx2 !== x || ny2 !== y) {
                 // 맨해튼 거리가 2이고 파티션 없이 응시자가 있는 경우
@@ -60,10 +60,10 @@ function solution(places) {
                 }
               }
             }
-                 } 
-               }
-           }
+          }
+        }
       }
-      return true;
+    }
+    
+    return true;
   }
-  
