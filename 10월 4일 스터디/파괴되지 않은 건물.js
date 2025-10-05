7a459a8bb682c7e3ecd94c3277e1a156055dcab9
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2025-10-05 12:09:39 +0900

    프로그래머스 파괴되지 않은 건물 문제를 공부한다.

---
+++ 10월 4일 스터디/파괴되지 않은 건물.js
@@ -0,0 +1,40 @@
+const prefixSum = (pSum, skill) => {
+  skill.forEach((row) => {
+    const [type, r1, c1, r2, c2, degree] = row;
+    const d = type === 1 ? -degree : degree;
+
+    pSum[r1][c1] += d;
+    pSum[r2 + 1][c1] += -d;
+    pSum[r1][c2 + 1] += -d;
+    pSum[r2 + 1][c2 + 1] += d;
+  });
+  //왼쪽에서 오른쪽으로 누적합
+  for (let r = 0; r < pSum.length; r++) {
+    for (let c = 1; c < pSum[0].length; c++) {
+      pSum[r][c] += pSum[r][c - 1];
+    }
+  }
+  //위에서 아래로 누적합
+  for (let r = 1; r < pSum.length; r++) {
+    for (let c = 0; c < pSum[0].length; c++) {
+      pSum[r][c] += pSum[r - 1][c];
+    }
+  }
+};
+
+function solution(board, skill) {
+  const pSum = Array.from({ length: board.length + 1 }, () =>
+    Array(board[0].length + 1).fill(0)
+  );
+  prefixSum(pSum, skill);
+
+  for (let r = 0; r < board.length; r++) {
+    for (let c = 0; c < board[0].length; c++) {
+      board[r][c] += pSum[r][c];
+    }
+  }
+
+  return board
+    .map((v) => v.filter((v2) => v2 > 0).length)
+    .reduce((acc, cur) => acc + cur);
+}
