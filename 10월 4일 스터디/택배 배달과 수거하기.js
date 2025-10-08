d4de753261276fa204ae4626ae19b57921507990
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2025-10-08 10:17:15 +0900

    택배 배달과 수거하기 문제를 복습한다.

--- 10월 4일 스터디/택배 배달과 수거하기.js
+++ 10월 4일 스터디/택배 배달과 수거하기.js
@@ -1,9 +1,10 @@
-function lastFun(arr) {
-  var len = arr.length;
-  for (var i = len - 1; i >= 0; i--) {
-    if (arr[i] !== 0) {
+function lastFun(arr){
+  var len  = arr.length;
+  for(var i = len-1; i >= 0;i--){
+    if(arr[i] !== 0){
       return i;
-    } else {
+    }
+    else {
       arr.pop();
     }
   }
@@ -11,58 +12,67 @@ function lastFun(arr) {
 }
 
 function delFun(deliveries, cap) {
-  let index = lastFun(deliveries);
-  let currentIndex = index;
-
-  while (index !== -1 && cap > 0) {
-    if (cap <= deliveries[index]) {
-      deliveries[index] -= cap;
-      cap = 0;
-      break;
-    } else if (cap > deliveries[index]) {
-      cap -= deliveries[index];
-      deliveries[index] = 0;
-      index--;
+    let index = lastFun(deliveries)
+    let currentIndex = index;
+    
+    while(index !== -1 && cap > 0) {
+        if(cap <= deliveries[index]) {
+            deliveries[index] -= cap
+            cap = 0;
+            break;
+        }
+        
+        if(cap > deliveries[index]) {
+            cap -= deliveries[index];
+            deliveries[index] = 0;
+            index--;
+        }
+        
     }
-  }
-
-  return currentIndex;
+    
+    return currentIndex;
 }
 
 function pickFun(deliveries, cap) {
-  let index = lastFun(deliveries);
-  let currentIndex = index;
-
-  while (index !== -1 && cap > 0) {
-    if (cap <= deliveries[index]) {
-      deliveries[index] -= cap;
-      cap = 0;
-      break;
-    } else if (cap > deliveries[index]) {
-      cap -= deliveries[index];
-      deliveries[index] = 0;
-      index--;
+    let index = lastFun(deliveries)
+    let currentIndex = index;
+    
+    while(index !== -1 && cap > 0) {
+        if(cap <= deliveries[index]) {
+            deliveries[index] -= cap;
+            cap = 0;
+            break;
+        }
+        
+        else if(cap > deliveries[index]) {
+            cap -= deliveries[index];
+            deliveries[index] = 0;
+            index--
+        }
+        
     }
-  }
-
-  return currentIndex;
+    
+    return currentIndex;
 }
 
-function solution(cap, _n, deliveries, pickups) {
-  let answer = 0;
-  //배열 마지막부터 0이 아닌곳 찾기 전부 0이면 -1을 리턴
+function solution(cap, n, deliveries, pickups) {
+    let answer = 0;
+    //배열 마지막부터 0이 아닌곳 찾기 전부 0이면 -1을 리턴
+    
+    while(true) {
+      let delLen = delFun(deliveries,cap) + 1;
+      let pickLen = pickFun(pickups,cap) + 1;
+      if(delLen === 0 && pickLen === 0){
+        return answer;
+      }
+      else if(delLen > pickLen){
+        answer += delLen * 2;
+      }
+      else{
+        answer += pickLen *2;
+      }
 
-  while (true) {
-    let delLen = delFun(deliveries, cap) + 1;
-    let pickLen = pickFun(pickups, cap) + 1;
-    if (delLen === 0 && pickLen === 0) {
-      return answer;
-    } else if (delLen > pickLen) {
-      answer += delLen * 2;
-    } else {
-      answer += pickLen * 2;
     }
-  }
-
-  return answer;
+    
+    return answer;
 }
