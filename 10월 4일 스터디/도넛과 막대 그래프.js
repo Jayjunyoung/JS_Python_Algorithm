143821d6ab6cc686934bb0bff050667e57c32180
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2025-10-12 12:50:28 +0900

    도넛과 막대 그래프 문제를 복습한다.

--- 10월 4일 스터디/도넛과 막대 그래프.js
+++ 10월 4일 스터디/도넛과 막대 그래프.js
@@ -1,12 +1,13 @@
 const getInfo = (edges) => {
   const info = edges.reduce((acc, cur) => {
+    // cur[0] 처리: 간선을 "준다"
     if (!acc.has(cur[0])) {
       acc.set(cur[0], [1, 0]);
     } else {
       const [give, receive] = acc.get(cur[0]);
       acc.set(cur[0], [give + 1, receive]);
     }
-
+    // cur[1] 처리: 간선을 "받는다"
     if (!acc.has(cur[1])) {
       acc.set(cur[1], [0, 1]);
     } else {
@@ -37,7 +38,6 @@ const checkGraph = (edges) => {
   }
 
   // 생성 정점의 간선 갯수에서 막대,도넛 8자 모양 빼주기
-  // 도넛 그래프의경우 진입: 1, 진출: 1 이라서 특정적으로 노드 카운트가 불가능
   res[1] = edges.get(res[0])[0] - res[2] - res[3];
 
   return res;
