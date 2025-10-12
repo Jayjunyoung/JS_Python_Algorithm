fb4a0920c6621839e817d936e43d3423ca2d32c7
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2025-10-12 13:09:31 +0900

    섬 연결하기 문제를 복습한다.

--- 10월 11일 스터디/섬 연결하기.js
+++ 10월 11일 스터디/섬 연결하기.js
@@ -36,6 +36,11 @@ function solution(n, costs) {
     const y = find(parent, edge[1]);
 
     if (x !== y) {
+      // 본인들의 루트 노드가 다르기 때문에 하나로 합치기
+      // 즉, 사이클이 생기지 않을 때 cost[2]를 더해준다
+      // 최소신장트리 : 사이클이 없는 연결 그래프
+      // V개의 노드를 V-1개의 간선으로 연결
+      // 크루스칼 알고리즘의 목적은 주어진 그래프에서 모든 노드를 가장 적은 비용으로 연결
       union(parent, rank, x, y);
       minCost += edge[2];
       edges += 1;
